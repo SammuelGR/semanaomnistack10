89d34f43b0e3cdbb8ba5af290fa3497a7410711f
Sammuel Reis <sammugr@hotmail.com>
2020-01-15 23:40:03 -0300

    initial index setup

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+// Route Params: request.params (Identificar um recurso na alteração ou remoção)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
 app.get('/', (request, response) => {
     return response.json({ message: 'Hello World' });
 });
