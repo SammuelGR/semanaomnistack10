292a82a6af94a085172add02ef0ee99e883acb18
Sammuel Reis <sammugr@hotmail.com>
2020-01-16 21:06:25 -0300

    setup mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://:@cluster0-dblys.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 app.use(express.json());
 
