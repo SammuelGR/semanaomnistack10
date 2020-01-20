8501a70e53aa8d3f7854eeadbe502939d13e23f3
Sammuel Reis <sammugr@hotmail.com>
2020-01-20 00:05:16 -0300

    main style with maps

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import MapView, { Callout, Marker } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
@@ -33,12 +33,48 @@ function Main() {
         return null;
     }
 
-    return <MapView initialRegion={currentRegion} style={styles.map} />
+    return (
+        <MapView initialRegion={currentRegion} style={styles.map}>
+            <Marker coordinate={{ latitude: -19.8987497, longitude: -44.0057082}}>
+                <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/23316518?s=460&v=4' }} />
+            
+                <Callout>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Sammuel Reis</Text>
+                        <Text style={styles.devBio}>Desenvolvedor React Native</Text>
+                        <Text style={styles.devTechs}>ReactJS, React Native, Node.js</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>    
+    );
 }
 
 const styles = StyleSheet.create({
     map: {
         flex: 1,
+    },
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF'
+    },
+    callout: {
+        width: 260,
+    },
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+    devTechs: {
+        marginTop: 5,
     },
 })
 
